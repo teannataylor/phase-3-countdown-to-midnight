65ecaa9f9db30632eaed9be1c7c31cf8ef9bf929
Teanna <teannamtaylor95@gmail.com>
2023-05-12 19:48:36 -0700

    commit tree cc8f72c1f7a8e54393a2f4c2b7de346c2b778608

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(x)
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -=1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(int=1)
+    sleep(int)
+end
+
+# while x < 10 puts "#{x} is less than 10" s+
